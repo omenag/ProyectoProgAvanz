47a638d1858e2a0144ecaadca84928427d027f3c
Oscar <Oscar@DELL_OMG>
2016-07-14 20:04:54 -0600

    Inicio de Proyecto

---
+++ ProjectPro/ProjectPro/ProjectPro/Models/ReCaptchaClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ProjectPro.Models
+{
+    public class ReCaptchaClass
+    {
+        private string m_Success;
+
+        [JsonProperty("success")]
+        public string Success
+        {
+            get { return m_Success; }
+            set { m_Success = value; }
+        }
+
+
+        private List<string> m_ErrorCodes;
+
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes
+        {
+            get { return m_ErrorCodes; }
+            set { m_ErrorCodes = value; }
+        }
+
+
+        public static string Validate(string EncodedResponse)
+        {
+            var client = new System.Net.WebClient();
+            string PrivateKey = "6LeZ6iQTAAAAANOMxENYtV_6x-Q4cehCLX_oM7qG";
+
+            var GoogleReply = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", PrivateKey, EncodedResponse));
+
+            var captchaResponse = JsonConvert.DeserializeObject<ReCaptchaClass>(GoogleReply);
+
+            return captchaResponse.Success;
+        }
+    }
+}
